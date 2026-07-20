e904a2d81298a2030b9d7501a23437c2a104db27
JosephIsNoob <unidentifiedperson117@gmail.com>
2026-07-20 23:05:45 +0800

    implement RBAC

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using DriveLingo.Database.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -16,6 +18,21 @@ namespace DriveLingo
             // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_PostAuthenticateRequest(object sender, EventArgs e)
+        {
+            if (Request.IsAuthenticated && Context.User != null)
+            {
+                if (Session != null && Session["CurrentUser"] is User)
+                {
+                    User currentUser = Session["CurrentUser"] as User;
+
+                    string[] roles = new[] { currentUser.Role.ToString() };
+
+                    Context.User = new GenericPrincipal(Context.User.Identity, roles);
+                }
+            }
         }
     }
 }
